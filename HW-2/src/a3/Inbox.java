6ccd62fb6305bcb2eeb079eff02b70ec645484ed
llau2 <Luisa Lau@luisas-computer.fritz.box>
2025-04-23 12:45:38 +0200

    aufgabe 3

---
+++ HW-2/src/a3/Inbox.java
@@ -0,0 +1,42 @@
+package a3;
+
+import java.util.ArrayList;
+
+public class Inbox {
+	// store all emails
+	private ArrayList<Mail> emails = new ArrayList<>();
+	
+	public ArrayList<Mail> getEmails() {
+		return emails;
+	}
+	public void setEmails(ArrayList<Mail> emails) {
+		this.emails = emails;
+	}
+	private void store(Mail mail) {
+		emails.add(mail);
+	}
+	public void print() {
+		for (int i = 0; i < emails.size(); i++) {
+			System.out.println(emails.get(i).isRead() + " | " + emails.get(i).getSubject() + " | " + emails.get(i).getSenderAddress() + " | " + emails.get(i).getDatetime());
+		}
+	}
+	
+	public void open(int index) {
+		if(index >= 0 && index < emails.size()) {
+			Mail mail = emails.get(index);
+			mail.markAsRead();
+			System.out.println( mail.print());
+		}
+	}
+	
+	public int countUnread() {
+		int unread = 0;
+		for (int i = 0; i < emails.size(); i++) {
+			if(!emails.get(i).isRead()) {
+				unread++;
+			}
+		}
+		
+		return unread;
+	}
+}
---
+++ HW-2/src/a3/Mail.java
@@ -0,0 +1,69 @@
+package a3;
+
+import java.time.LocalDateTime;
+
+public class Mail {
+	private String senderAddress;
+	private String subject;
+	private String message;
+	private LocalDateTime datetime;
+	private boolean read;
+	
+	public Mail(String senderAddress, String subject, String message) {
+		this.senderAddress = senderAddress;
+		this.subject = subject;
+		this.message = message;
+		this.datetime = LocalDateTime.now();
+		this.read = false;
+	}
+	
+	public String getSenderAddress() {
+		return senderAddress;
+	}
+
+	public void setSenderAddress(String senderAddress) {
+		this.senderAddress = senderAddress;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public LocalDateTime getDatetime() {
+		return datetime;
+	}
+
+	public void setDatetime(LocalDateTime datetime) {
+		this.datetime = datetime;
+	}
+
+	public boolean isRead() {
+		return read;
+	}
+
+	public void setRead(boolean read) {
+		this.read = read;
+	}
+
+	public void markAsRead() {
+		if (!read) {
+			read = true;
+		}
+	}
+	public String print() {
+		return subject + " from " + senderAddress + " on " + datetime + ": " + message + ".";
+		
+	}
+}
