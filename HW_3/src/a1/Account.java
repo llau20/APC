cc5c1ce6082474f7219c8b685e98c11f57302ec1
llau2 <Luisa Lau@172.29.226.227>
2025-04-24 10:34:43 +0200

    anfang 3-1

---
+++ HW_3/src/a1/Account.java
@@ -0,0 +1,21 @@
+package a1;
+
+public class Account {
+	public String owner;
+	private double balance;
+	protected int pin;
+	String internalNote;
+	
+	
+	public double getBalance() {
+		return balance;
+	}
+
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+	
+	
+
+}
